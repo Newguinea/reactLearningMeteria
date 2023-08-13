2eca4f70806d39e430e6de8ba793ddb3933c7bfa
Newguinea <moshuiheiyoutiao@gmail.com>
2023-08-13 14:38:09 +0800

    to lesson 67

--- 04-steps/04my_steps/src/App.js
+++ 04-steps/04my_steps/src/App.js
@@ -9,19 +9,28 @@ const messages = [
 const buttonStyle = { backgroundColor: "#7950f2", color: "#fff" };
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
   return (
     <div>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
         &times;
       </button>
       {isOpen && (
