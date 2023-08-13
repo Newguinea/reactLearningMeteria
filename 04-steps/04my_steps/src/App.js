bf0b880dda9e2c6eeaf65070f7f36d10672011f4
Newguinea <moshuiheiyoutiao@gmail.com>
2023-08-13 14:10:46 +0800

    to course 63, 04 steps

---
+++ 04-steps/04my_steps/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const buttonStyle = { backgroundColor: "#7950f2", color: "#fff" };
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button style={buttonStyle} onClick={handlePrevious}>
+              Previous
+            </button>
+            <button style={buttonStyle} onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
