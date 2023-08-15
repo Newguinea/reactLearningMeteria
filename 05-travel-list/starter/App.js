143b0a28cc9a955c27d44d6f42a8b4b1ecc8df9a
Newguinea <moshuiheiyoutiao@gmail.com>
2023-08-15 13:04:01 +0800

    lesson 75

--- 05-travel-list/starter/App.js
+++ 05-travel-list/starter/App.js
@@ -1,9 +1,27 @@
 import { useState } from "react";
 
-export default function Form({ onAddItems }) {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🪖 Far Away 😊</h1>;
+}
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
-
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -12,14 +30,13 @@ export default function Form({ onAddItems }) {
     const newItem = { description, quantity, packed: false, id: Date.now() };
 
     onAddItems(newItem);
-
     setDescription("");
     setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip?</h3>
+      <h3>What do you need for your trip?</h3>
       <select
         value={quantity}
         onChange={(e) => setQuantity(Number(e.target.value))}
@@ -40,3 +57,26 @@ export default function Form({ onAddItems }) {
     </form>
   );
 }
+
+function PackingList() {
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} key={item.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button className="remove">✖️</button>
+    </li>
+  );
+}
+function Stats() {}
